38b93434d3f3f7793c9f69c689358cf49ecf956d
jschroeder330 <jschroeder@linkmedia360.com>
2018-09-16 20:28:35 -0400

    Update project files after second section

--- flask/social_network/project/app.py
+++ flask/social_network/project/app.py
@@ -1,5 +1,15 @@
-from flask import Flask, g
-from flask.ext.login import LoginManager
+from flask import (Flask,
+                    g,
+                    flash,
+                    render_template,
+                    redirect,
+                    url_for)
+import forms
+from flask.ext.bcrypt import check_password_hash
+from flask.ext.login import (LoginManager,
+                            login_user, #logs in user
+                            logout_user,
+                            login_required)
 
 import models
 
@@ -12,6 +22,7 @@ app.secret_key = "asldfjalkghhgwiuerqhgjkdhnaasjdfhsalkfaubg"
 
 login_manager = LoginManager()
 login_manager.init_app(app) # sets up the login manager for our app
+# note: calling this 'login' requires the url be /login
 login_manager.login_view = 'login' # if not logged in, redirect them to the login view
 
 # function for login_manager to use to lookup a User
@@ -34,12 +45,62 @@ def after_request(response):
     g.db.close() # close global db connection
     return response
 
+@app.route('/register', methods=('GET', 'POST'))
+def register():
+    form = forms.RegisterForm()
+    # flash(form.validate_on_submit())
+    if form.validate_on_submit():  # detects that user has submitted a valid form
+        flash("Form Submission Successful.", "success")  # message and message category
+        models.User.create_user(
+                username=form.username.data,
+                email=form.email.data,
+                password=form.password.data # shouldn't we hash this?
+                )
+        return redirect(url_for('index'))
+    return render_template('register.html', form=form)
+
+
+@app.route('/login', methods=('GET', 'POST'))
+def login():
+    form = forms.LoginForm()
+    if form.validate_on_submit():
+        try:
+            user = models.User.get(models.User.email == form.email.data)
+        except models.DoesNotExist:
+            # keep things ambiguous to deter hackers
+            flash("Your email or password doesn't match!", "error")
+        else:
+            if check_password_hash(user.password, form.password.data):
+                login_user(user)
+                flash("You've been logged in!", "success")
+                return redirect(url_for('index'))
+            else:
+                flash("Your email or password doesn't match!", "error")
+    return render_template('login.html', form=form)
+
+
+@app.route('/logout')
+@login_required  # you need to be logged into see this view
+def logout():
+    logout_user()  # terminate the session - deletes cookie
+    flash("You've been logged out. Come back soon!", "success")
+    return redirect(url_for('index'))
+
+
+@app.route('/')
+def index():
+    return("Index")
+
+
 if __name__ == '__main__':
-    models.initialize() # initialize db
-    models.User.create_user( # create a dummy user using create_user to encrypt password
-            name="Jake",
-            email="schroeder.marketing@outlook.com",
-            password="password",
-            admin=True
-            )
+    models.initialize()  # initialize db
+    try:
+        models.User.create_user(  # create a dummy user using create_user
+                username="Jake",
+                email="schroeder.marketing@outlook.com",
+                password="password",
+                admin=True
+                )
+    except ValueError:
+        pass
     app.run(debug=DEBUG, host=HOST, port=PORT)
--- flask/social_network/project/forms.py
+++ flask/social_network/project/forms.py
@@ -15,27 +15,27 @@ def name_exists(form, field):
     if User.select().where(User.username == field.data).exists():
         raise ValidationError("User with that name already exists.")
 
+
 def email_exists(form, field):
     if User.select().where(User.email == field.data).exists():
-        raise ValidationError("User with that name already exists.")
+        raise ValidationError("User with that email already exists.")
 
 
 class RegisterForm(Form):
     username = StringField(
-            'UserName', # field(label)
+            'UserName',  # field(label)
             validators=[
                 DataRequired(),
                 Regexp(
-                    r'^a-zA-Z0-9_]+$',
-                    message=("Username should be one word, "
-                            "letters numbers and underscores")
-                    ),
+                    r'^[a-zA-Z0-9_]+$',
+                    message="""Username should be one word,
+                            letters numbers and underscores"""),
                 name_exists
             ])
     email = StringField(
             'Email',
             validators=[
-                DataRequired(), # there must be data
+                DataRequired(),  # there must be data
                 Email(),
                 email_exists
             ])
@@ -48,4 +48,9 @@ class RegisterForm(Form):
             ])
     password2 = PasswordField(
             'Confirm Password',
-            validators=[DataRequired])
+            validators=[DataRequired()])
+
+
+class LoginForm(Form):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
